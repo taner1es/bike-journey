5e61c6605260d08ab8a4c6eb93f1f2166e2575c7
Taner Esmeroğlu <taner.esmrglu@gmail.com>
2019-05-18 17:39:20 +0300

    Camera Zoom Adjustment Script Added
    main camera zooms according to current aspect ratio

---
+++ Assets/Scripts/ScalableCamera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class ScalableCamera : MonoBehaviour
+{
+    private void Start()
+    {
+        float PIXELS_TO_UNITS = 30; // 1:1 ratio of pixels to units
+
+        float TARGET_WIDTH = 1920f;
+        float TARGET_HEIGHT = 1080.0f;
+
+        float desiredRatio;
+        float currentRatio;
+        desiredRatio = TARGET_WIDTH / TARGET_HEIGHT;
+        currentRatio = (float)Screen.width / (float)Screen.height;
+
+        PIXELS_TO_UNITS = currentRatio * 10.11235955056f;
+        if (currentRatio >= desiredRatio)
+        {
+            // Our resolution has plenty of width, so we just need to use the height to determine the camera size
+            Camera.main.orthographicSize = TARGET_HEIGHT / 4 / PIXELS_TO_UNITS;
+        }
+        else
+        {
+            // Our camera needs to zoom out further than just fitting in the height of the image.
+            // Determine how much bigger it needs to be, then apply that to our original algorithm.
+            float differenceInSize = desiredRatio / currentRatio;
+            Camera.main.orthographicSize = TARGET_HEIGHT / 4 / PIXELS_TO_UNITS * differenceInSize;
+        }
+    }
+}

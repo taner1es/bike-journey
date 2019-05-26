54e4b552bdb95000c086f70ddd3d859e7b1ba771
Taner Esmeroğlu <taner.esmrglu@gmail.com>
2019-05-26 20:09:03 +0300

    Matching Game environment drew and implemented.

---
+++ Assets/Scripts/MatchingAreaTrigger.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class MatchingAreaTrigger : MonoBehaviour
+{
+    public Sprite closedSprite;
+    public GameObject closedTextGO;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(gameObject.name == collision.gameObject.name)
+        {
+            Destroy(collision.gameObject);
+
+            gameObject.GetComponent<SpriteRenderer>().sprite = closedSprite;
+
+            string text = transform.GetChild(0).GetComponent<TextMeshPro>().text;
+
+            Destroy(transform.GetChild(0).gameObject);
+
+            GameObject newTextGO = Instantiate(closedTextGO, transform);
+            newTextGO.GetComponent<TextMeshPro>().SetText(text);
+
+            gameObject.GetComponentInParent<HorizontalLayoutGroup>().enabled = false;
+            gameObject.GetComponentInParent<HorizontalLayoutGroup>().enabled = true;
+
+            /*Destroy(collision.gameObject);
+            //gameObject.GetComponent<Renderer>().material.color = Color.green;
+
+            gameObject.GetComponent<SpriteRenderer>().sprite = matchingAreaClosedPrefab.GetComponent<SpriteRenderer>().sprite;
+            gameObject.GetComponentInChildren<RectTransform>().localPosition = matchingAreaClosedPrefab.GetComponent<RectTransform>().localPosition;
+            
+
+
+            Destroy(gameObject.GetComponent<Collider2D>());
+
+
+
+
+            //gameObject.GetComponent<Collider2D>().enabled = false;
+            */
+        }
+    }
+}
